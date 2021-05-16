7f58b3d78f51ccdff10d27333ecbf18d23c68e65
StevenWuTG <stevenwu91@icloud.com>
2021-05-16 12:27:07 -0400

    CI start

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Updated repo</h1>
+        <h1>welcome to the continuous site</h1>
       </header>
     </div>
   );
